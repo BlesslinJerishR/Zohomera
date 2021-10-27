5936e7c724b68d4edc27963efb5abfa4757d2652
Mastermindx33 <mastermindx33@gmail.com>
2021-10-27 19:38:28 +0530

    deploy @build

--- static_gangs.py
+++ static_gangs.py
@@ -1,9 +1,19 @@
-# Gang
 gang = []
-times = int(input("Enter the team size : "))
+times = int(input("Enter the number of times : "))
+print(f"Enter the {times} team members names : ")
+# Gang
 id = 1
 for i in range(times):
     thug = int(input(f"Enter the power of Thug id {id} : "))
     id += 1
     gang.append(thug)   
-print(f"We are the leaders {max(gang)} , {str(gang[-1])}")
+leaders = []
+gangx = gang[1:]
+for i in gang:
+    for j in gangx:
+        if i > j:
+            leaders.append(i)
+        else:
+            continue
+        
+print(list(set(leaders)))
