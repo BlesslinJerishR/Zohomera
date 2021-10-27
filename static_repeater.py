ac0543bcbd5b986bf1486e634a3b56eb35a7b417
Mastermindx33 <mastermindx33@gmail.com>
2021-10-27 20:44:57 +0530

    Update static_repeater.py

--- static_repeater.py
+++ static_repeater.py
@@ -10,6 +10,6 @@ print("I am first repeater in my family : ",l_uniq[0])
 def repeater(word):
     garage_0 = [x for x in word]
     garage_1 = set()
-    unix, l_uniq = [], []
+    unix = []
     l_uniq = [x for x in garage_0 if x not in unix or garage_1.add(x) ]
     print("I am first repeater in my family : ",l_uniq[0])
